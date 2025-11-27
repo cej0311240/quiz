1c94aa825b98d0dd84eced26820c02028a875509
최은지 <cej0311240@gmail.com>
2025-11-27 17:59:57 +0900

    index.html

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,9 +3,15 @@
 const Results = ({onReStart}) => {
   return (
     <div id='result'>
-      <p>퀴즈 종료</p>
+      <div className="end">
+      <p>총 점수는?</p>
+      <h2>｛100점｝</h2>
+      <p>〈지식이 탄탄하시군요~!/〉</p>
       <button onClick={onReStart}>다시 시작</button>
+      </div>
+      <img src="./img/트로피-금색.png"/>
     </div>
+    
   )
 }
 
